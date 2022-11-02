9cc3125e19bd86219b0656c7a60e816e730ed8e4
anamoraisdev <anamorais.dev@gmail.com>
2022-11-02 16:45:32 -0300

    Alterando perguntas

--- quiz.py
+++ quiz.py
@@ -39,46 +39,46 @@ if resposta1 == "B":
 else:
      print("Incorreto!")
 
-print ("Qual o nome do personagem principal do filme Poderoso Chefão 1? \n (A)Michael Corleone \n (B) Vitor Corleone \n(C) Sonny Corleone \n (D) Fredo Corleone ")
+print ("Qual criança de A Fantástica Fábrica de Chocolate achou o primeiro Bilhete Dourado?\n(A)Augustos Gloop\n(B)Veruca Salt\n(C)Violet Beauregarde\n(D)Mike Teave)
 resposta1 = input ("resposta:")
 
-if resposta1 == "B":
+if resposta1 == "A":
     print ("Correto!")
     score = score + 1 
 else:
      print("Incorreto!")
 
-print ("Qual o nome do personagem principal do filme Poderoso Chefão 1? \n (A)Michael Corleone \n (B) Vitor Corleone \n(C) Sonny Corleone \n (D) Fredo Corleone ")
+print ("Qual destes filmes da Disney foi lançado primeiro?\n(A)O rei leão\n(B)Toy story\n(C)Mulan\n(D)Pocahontas")
 resposta1 = input ("resposta:")
 
-if resposta1 == "B":
+if resposta1 == "A":
     print ("Correto!")
     score = score + 1 
 else:
      print("Incorreto!")
 
-print ("Qual o nome do personagem principal do filme Poderoso Chefão 1? \n (A)Michael Corleone \n (B) Vitor Corleone \n(C) Sonny Corleone \n (D) Fredo Corleone ")
+print ("Por qual destes filmes Meryl Streep NÃO ganhou um Oscar?\n(A)A dama de ferro\n(B)Kramer vs Kramer\n(C)A escolha de Sofia\n(D)Duvida")
 resposta1 = input ("resposta:")
 
-if resposta1 == "B":
+if resposta1 == "D":
     print ("Correto!")
     score = score + 1 
 else:
      print("Incorreto!")
 
-print ("Qual o nome do personagem principal do filme Poderoso Chefão 1? \n (A)Michael Corleone \n (B) Vitor Corleone \n(C) Sonny Corleone \n (D) Fredo Corleone ")
+print ("Qual destes personagens terminou o filme/franquia ainda vivo? \n (A)Dobby-Harry Potter \n (B)Bill Muray-Zumbilândia \n(C)Xerife dewey Riley-Panico \n (D)Jenny-Forrest Gump ")
 resposta1 = input ("resposta:")
 
-if resposta1 == "B":
+if resposta1 == "C":
     print ("Correto!")
     score = score + 1 
 else:
      print("Incorreto!")
 
-print ("Qual o nome do personagem principal do filme Poderoso Chefão 1? \n (A)Michael Corleone \n (B) Vitor Corleone \n(C) Sonny Corleone \n (D) Fredo Corleone ")
+print ("Qual foi o primeiro filme a mostrar uma descarga em um vaso sanitário?\n(A)Psicose\n(B)Casablanca \n(C)O mágico de Oz\n(D)O que terá acontecido a Baby Jane")
 resposta1 = input ("resposta:")
 
-if resposta1 == "B":
+if resposta1 == "A":
     print ("Correto!")
     score = score + 1 
 else:
