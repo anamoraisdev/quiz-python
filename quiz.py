ec3cdb561e429f24585c24fe8dd00ba620407634
anamoraisdev <anamorais.dev@gmail.com>
2022-11-01 21:08:37 -0300

    Fazendo contagem de pontos

--- quiz.py
+++ quiz.py
@@ -1,4 +1,5 @@
 import time 
+score = 0
 
 senha = input ("Identificação:")
 if senha == "maryfooti":
@@ -26,7 +27,7 @@ def countdown(t):
     print('VALENDO!!') 
   
   
-t = (10) 
+t = (5) 
 countdown(int(t)) 
 
 print ("Qual o nome do personagem principal do filme Poderoso Chefão 1? \n (A)Michael Corleone \n (B) Vitor Corleone \n(C) Sonny Corleone \n (D) Fredo Corleone ")
@@ -34,6 +35,115 @@ resposta1 = input ("resposta:")
 
 if resposta1 == "B":
     print ("Correto!")
+    score = score + 1 
 else:
      print("Incorreto!")
+
+print ("Qual o nome do personagem principal do filme Poderoso Chefão 1? \n (A)Michael Corleone \n (B) Vitor Corleone \n(C) Sonny Corleone \n (D) Fredo Corleone ")
+resposta1 = input ("resposta:")
+
+if resposta1 == "B":
+    print ("Correto!")
+    score = score + 1 
+else:
+     print("Incorreto!")
+
+print ("Qual o nome do personagem principal do filme Poderoso Chefão 1? \n (A)Michael Corleone \n (B) Vitor Corleone \n(C) Sonny Corleone \n (D) Fredo Corleone ")
+resposta1 = input ("resposta:")
+
+if resposta1 == "B":
+    print ("Correto!")
+    score = score + 1 
+else:
+     print("Incorreto!")
+
+print ("Qual o nome do personagem principal do filme Poderoso Chefão 1? \n (A)Michael Corleone \n (B) Vitor Corleone \n(C) Sonny Corleone \n (D) Fredo Corleone ")
+resposta1 = input ("resposta:")
+
+if resposta1 == "B":
+    print ("Correto!")
+    score = score + 1 
+else:
+     print("Incorreto!")
+
+print ("Qual o nome do personagem principal do filme Poderoso Chefão 1? \n (A)Michael Corleone \n (B) Vitor Corleone \n(C) Sonny Corleone \n (D) Fredo Corleone ")
+resposta1 = input ("resposta:")
+
+if resposta1 == "B":
+    print ("Correto!")
+    score = score + 1 
+else:
+     print("Incorreto!")
+
+print ("Qual o nome do personagem principal do filme Poderoso Chefão 1? \n (A)Michael Corleone \n (B) Vitor Corleone \n(C) Sonny Corleone \n (D) Fredo Corleone ")
+resposta1 = input ("resposta:")
+
+if resposta1 == "B":
+    print ("Correto!")
+    score = score + 1 
+else:
+     print("Incorreto!")
+print ("Qual o nome do personagem principal do filme Poderoso Chefão 1? \n (A)Michael Corleone \n (B) Vitor Corleone \n(C) Sonny Corleone \n (D) Fredo Corleone ")
+resposta1 = input ("resposta:")
+
+if resposta1 == "B":
+    print ("Correto!")
+    score = score + 1 
+else:
+     print("Incorreto!")
+
+print ("Qual o nome do personagem principal do filme Poderoso Chefão 1? \n (A)Michael Corleone \n (B) Vitor Corleone \n(C) Sonny Corleone \n (D) Fredo Corleone ")
+resposta1 = input ("resposta:")
+
+if resposta1 == "B":
+    print ("Correto!")
+    score = score + 1 
+else:
+     print("Incorreto!")
+
+print ("Qual o nome do personagem principal do filme Poderoso Chefão 1? \n (A)Michael Corleone \n (B) Vitor Corleone \n(C) Sonny Corleone \n (D) Fredo Corleone ")
+resposta1 = input ("resposta:")
+
+if resposta1 == "B":
+    print ("Correto!")
+    score = score + 1 
+else:
+     print("Incorreto!")
+
+print ("Qual o nome do personagem principal do filme Poderoso Chefão 1? \n (A)Michael Corleone \n (B) Vitor Corleone \n(C) Sonny Corleone \n (D) Fredo Corleone ")
+resposta1 = input ("resposta:")
+
+if resposta1 == "B":
+    print ("Correto!")
+    score = score + 1 
+else:
+     print("Incorreto!")
+
+
+print ("O Quiz acabou!!! \n A sua pontuação é ...")
+def countdown(t): 
+    
+    while t: 
+        mins, secs = divmod(t, 60) 
+        timer = '{:02d}:{:02d}'.format(mins, secs) 
+        print(timer, end="\r") 
+        time.sleep(1) 
+        t -= 1
+      
+t = (5) 
+countdown(int(t)) 
+
+
+if score == int("10"):
+    print(f"{score} de 10 perguntas")
+    print("Arrasou!! Você alcançou a pontuação máxima.")
+elif score >= int("7"):
+    print(f"{score} de 10 perguntas")
+    print("Parabéns! Voce se saiu muito bem.")
+else:
+    print(f"{score} de 10 perguntas")
+    print("Você não alcançou pontuação suficiente para o próximo nível. Mas, não fique triste, voce pode tentar de novo reiniciando o programa.")
+
+
+
 
